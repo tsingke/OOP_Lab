975f51fd3c6f359314c7dc66e7b2759456153ca6
Qingke Zhang <tsingke@hotmail.com>
2019-10-31 21:15:15 +0800

    Add files via upload

---
+++ chapt3_class/friend-global function.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+
+using namespace std;
+
+class date
+{
+    int year;
+    int month;
+    int day;
+
+public:
+    date(int y,int m,int d) ;
+    ~date();
+    void display();
+    friend void birthdayCheck(date &r1,date &r2);
+};
+
+date::date(int y, int m, int d)
+{
+   this->year=y;
+    this->month=m;
+            this->day=d;
+   cout<<"this is a constructor function " <<endl;
+}
+
+date::~date()
+{
+   cout<<"this is a deconstrucor"<<endl ;
+}
+
+void date::display()
+{
+   cout<<year<<"年-"<<month<<"月-"<<day<<"日"<<endl;
+}
+
+void birthdayCheck(date &r1,date &r2)
+{
+
+    if((r1.year==r2.year)&&(r1.month==r2.month)&&(r1.day==r2.day))
+    {
+        cout<<"是同一天生日"<<endl;
+    }
+    else
+    {
+        cout<<"不是同一天生日"<<endl;
+    }
+}
+
+int main()
+{
+
+
+    date zhang(2000,10,1);
+    date wang(2000,10,1);
+
+    zhang.display();
+
+    wang.display();
+
+
+    birthdayCheck(zhang,wang);
+
+    return 0;//
+
+}
