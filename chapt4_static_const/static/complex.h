2a4990da3d029d34fc3d28a723756c5b8e052840
Qingke Zhang <tsingke@hotmail.com>
2019-11-12 13:08:29 +0800

    Update complex.h

--- chapt4_static_const/static/complex.h
+++ chapt4_static_const/static/complex.h
@@ -18,7 +18,7 @@ public:
   static void showcount();//
 };
 
-//int complex::s=0;    //类外初始化
-//int complex::count=0;//类外初始化;
+//int complex::s=0;    //类外初始化,务必注意,如果将类采用多文件编程时,静态成员变量的定义不要放在.h文件中,放在.cpp文件中,否则会引发多重定义问题.
+//int complex::count=0;//类外初始化,必注意,如果将类采用多文件编程时,静态成员变量的定义不要放在.h文件中,放在.cpp文件中,否则会引发多重定义问题.
 
 #endif // COMPLEX_H
