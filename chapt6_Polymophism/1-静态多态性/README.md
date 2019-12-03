e962ea3fb685670f19fd87304cd4196fbf454657
Qingke Zhang <tsingke@hotmail.com>
2019-12-03 18:15:37 +0800

    一码知天下

---
+++ chapt6_Polymophism/1-静态多态性/README.md
@@ -0,0 +1,57 @@
+```c++
+#ifndef COMPLEX_H
+#define COMPLEX_H
+
+#include <iostream>
+using namespace std;
+
+class complex
+{
+private:
+    double real;
+    double imag;
+
+public:
+    //===================静态多态: 函数重载========================
+    complex();
+    complex(double r, double i);//构造函数
+    complex(const complex &r);
+     ~complex();
+
+    void print();
+    void print()const;
+
+
+
+    //===============静态多态: 运算符重载1-成员函数法===============
+
+     //a.重载加法运算符+(双目运算符)
+    complex operator+(const complex &other);
+
+
+    //b.重载赋值运算符=(双目运算符)-类内重载
+
+    complex operator=(const complex &other);
+
+     //c.重载前++运算符(单目运算符)-类内重载,类外重载均可
+    complex operator++();
+
+
+     //d.重载后++运算符(单目运算符)-类内重载
+    complex operator++(int);
+
+
+    //e.重载函数调用运算符()运算符(单目运算符)-类内重载
+    double operator()();
+
+   //===============静态多态: 运算符重载2-友元函数法===============
+
+    //-重载输出运算符<< : 双目运算符,格式"cout<<" 左侧是一个输出流对象cout"
+    friend ostream& operator<<(ostream &out, complex &t);
+
+};
+
+
+#endif // COMPLEX_H
+
+```
---
+++ chapt6_Polymophism/1-静态多态性/complex.cpp
@@ -0,0 +1,92 @@
+#include "complex.h"
+
+
+
+//-----------------------成员函数-------------------------
+
+complex::complex()
+{
+
+}
+
+complex::complex(double r, double i)
+{
+    this->real =r;
+    this->imag =i;
+}
+
+complex::complex(const complex &r)
+{
+    this->real =r.real;
+    this->imag = r.imag;
+}
+
+complex::~complex()
+{
+
+}
+
+void complex::print() const
+{
+
+
+}
+
+complex complex::operator+(const complex &other)
+{
+
+    complex temp;
+
+    temp.real = this->real + other.real;
+    temp.imag = this->imag + other.imag;
+
+    return  temp;
+
+}
+
+//赋值运算符=
+complex complex::operator=(const complex &other)
+{
+    this->real = other.real;
+    this->imag = other.imag;
+}
+
+//前++
+complex complex::operator++()
+{
+    ++this->real;
+    ++this->imag;
+
+    return *this;
+
+}
+
+//后++
+complex complex::operator++(int)
+{
+    complex temp;//
+
+    temp.real = this->real++;
+    temp.imag=this->imag++;
+
+    return  temp;
+
+}
+
+//函数调用符
+double complex::operator()()
+{
+    double sum = this->real * this->real + this->imag *this->imag;
+
+    return  sum;
+
+}
+
+ostream & operator<<(ostream &out, complex &t)
+{
+    out<<t.real<<"+" <<t.imag<<"i"<<endl;
+}
+
+//-----------------------友元函数-------------------------
+
+
